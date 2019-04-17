08dca8c2adba1be5363ab7708329982ecc930b01
Angelussz <38799735+Angelussz@users.noreply.github.com>
2019-04-17 10:59:55 -0500

    Add files via upload

---
+++ p9.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int na;
+	int nb;
+	int nc;
+	for (na=1; na<=500; na++)
+	{
+		for (nb=1; nb<=500; nb++)
+		{
+			nc=1000-nb-na;
+			if (na*na+nb*nb-nc*nc == 0 && na<nb )
+			{
+				cout<<na<<" "<<nb<<" "<<" "<<nc<<" "<<na*nb*nc<<endl;
+			}
+		}
+	}
+	return 0;
+}
